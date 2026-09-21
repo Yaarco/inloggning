d13243aafaf1addb7d85fb92a215fe40ffe9ad11
Yaarco <marco.romin@gmail.com>
2026-09-21 10:32:55 +0200

    styling och klar js

--- script.js
+++ script.js
@@ -1,11 +1,53 @@
 const loginBtn = document.getElementById("login-btn")
 const logoutBtn = document.getElementById("logout-btn")
 const loginForm = document.getElementById("login-form")
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+
+const dashboardBox = document.getElementById("dashboard")
+const loginBox = document.getElementById("login")
 
 const alertBox = document.getElementById("alert")
-const statusBox = document.getElementById("status")
+
+const username = "Kalle"
+const password = "qwe123"
+
+function showSection(name, value){
+    return document.getElementById(name).style.display = value?"block":"none"
+}
+
+function logout(){
+    showSection("dashboard", false)
+    showSection("login", true)
+    alertBox.innerText=""
+    localStorage.removeItem("loggedin")
+}
+
+function login(){
+    showSection("dashboard", true)
+    showSection("login", false)
+    document.querySelector("span").innerText=username
+    localStorage.setItem("loggedin", true)
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    if(Boolean(localStorage.getItem("loggedin"))===true){
+        login()
+    }
+})
+
 
 loginForm.addEventListener("submit", e=>{
     e.preventDefault() // så att inte en post eller get req skickas
-    console.log(e)
+    const inputPassword = passwordInput.value
+    const inputUsername = usernameInput.value
+    if(username===inputUsername && password===inputPassword){
+        login()
+    }else {
+        alertBox.innerText="Felaktiga inloggningsuppgifter"
+    }
+})
+
+logoutBtn.addEventListener("click", ()=>{
+    logout()
 })
