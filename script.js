469444ec0fb30d0e557739cbe255e73074e50434
Yaarco <marco.romin@gmail.com>
2026-09-23 16:48:44 +0200

    Ändrat design och cleanat upp kod samt lagt fler kommentarer

--- script.js
+++ script.js
@@ -1,47 +1,39 @@
-const loginBtn = document.getElementById("login-btn")
-const logoutBtn = document.getElementById("logout-btn")
-const loginForm = document.getElementById("login-form")
-const usernameInput = document.getElementById('username');
-const passwordInput = document.getElementById('password');
-
-const dashboardBox = document.getElementById("dashboard")
-const loginBox = document.getElementById("login")
-
-const alertBox = document.getElementById("alert")
-
-const username = "Kalle"
-const password = "qwe123"
+const logoutBtn = document.getElementById("logout-btn"),
+loginForm = document.getElementById("login-form"),
+usernameInput = document.getElementById('username'),
+passwordInput = document.getElementById('password'),
+alertBox = document.getElementById("alert"),
+username = "Kalle",
+password = "qwe123"
 
 function showSection(name, value){
-    return document.getElementById(name).style.display = value?"block":"none"
+    document.getElementById(name).style.display = value?"block":"none" // anger om den är synlig eller inte beroende på om value är true eller false
 }
 
 function logout(){
-    showSection("dashboard", false)
-    showSection("login", true)
-    alertBox.innerText=""
-    localStorage.removeItem("loggedin")
+    showSection("dashboard", false) // dölj dashboard sektionen
+    showSection("login", true) // visa login sektionen
+    alertBox.innerText="" // ta bort eventuell "felaktigt lösenord" text
+    localStorage.removeItem("loggedin") // ta bort att användaren är inloggad från lagring
 }
 
 function login(){
-    showSection("dashboard", true)
-    showSection("login", false)
-    document.querySelector("span").innerText=username
-    localStorage.setItem("loggedin", true)
+    showSection("dashboard", true) // visa dashboard sektionen
+    showSection("login", false) //dölj login sektionen
+    document.querySelector("span").innerText=username //sätter in namnet i Välkommen texten
+    localStorage.setItem("loggedin", true) // sparar en bool istället för att lägga både lösenord och användarnamn i localstorage eftersom det bara finns ett giltigt ändå
+}
+if(Boolean(localStorage.getItem("loggedin"))){ // localstorage sparar bara strings, så vi får omvandla "true" till boolean
+    login()
 }
 
-document.addEventListener("DOMContentLoaded", ()=>{
-    if(Boolean(localStorage.getItem("loggedin"))===true){
-        login()
-    }
-})
 
 
-loginForm.addEventListener("submit", e=>{
-    e.preventDefault() // så att inte en post eller get req skickas
+loginForm.addEventListener("submit", e=>{ //när formen submittas (via knappen) så körs följande kod
+    e.preventDefault() // så att inte en post eller get request skickas
     const inputPassword = passwordInput.value
     const inputUsername = usernameInput.value
-    if(username===inputUsername && password===inputPassword){
+    if(username===inputUsername && password===inputPassword){ // om användarnamn och lösenord stämmer med det sparade
         login()
     }else {
         alertBox.innerText="Felaktiga inloggningsuppgifter"
